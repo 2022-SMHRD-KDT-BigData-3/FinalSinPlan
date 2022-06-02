4705a3c227f81a31f08ca7e7a90ca773bf250c4c
smhrd <smhrd@DESKTOP-SSEAMBB>
2022-06-02 11:46:57 +0900

    db 연결 완료!

--- skinplan/src/main/java/kr/main/controller/MainController.java
+++ skinplan/src/main/java/kr/main/controller/MainController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.log4j.Log4j;
+
 @Controller
 public class MainController {
 
