05a4f4e1ab4aa2ad59691a4f90fef21e4cd51f7b
smhrd <smhrd@DESKTOP-SSEAMBB>
2022-06-02 17:17:43 +0900

    테이블 user_info, skin_type 생성

---
+++ skinplan/src/main/webapp/resources/sql.sql
@@ -0,0 +1,16 @@
+CREATE TABLE skin_type(
+ skin_id NUMBER(3),
+ skin_type VARCHAR2(10) NOT NULL,
+ skin_con VARCHAR2(30) NOT NULL,
+ CONSTRAINT skin_id_pk PRIMARY KEY(skin_id));
+
+CREATE TABLE user_info(
+ email VARCHAR2(30),
+ user_name VARCHAR2(10) NOT NULL,
+ user_pwd VARCHAR2(20) NOT NULL,
+ user_tel VARCHAR2(15),
+ skin_id NUMBER(3),
+ CONSTRAINT user_email_pk PRIMARY KEY(email),
+ CONSTRAINT user_skin_fk FOREIGN KEY(skin_id) REFERENCES skin_type(skin_id));
+ 
+ select * from user_info;
